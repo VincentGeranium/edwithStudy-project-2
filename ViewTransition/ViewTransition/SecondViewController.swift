0290fb5b33f6620246a6295bb69b566580b4c738
김광준 <kwangjun3952@gmail.com>
2019-09-23 19:32:26 +0900

    내비게이션 인터페이스 만들기 실습

---
+++ ViewTransition/ViewTransition/SecondViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SecondViewController.swift
+//  ViewTransition
+//
+//  Created by 김광준 on 23/09/2019.
+//  Copyright © 2019 VincentGeranium. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didTappedPopBtn() {
+        self.navigationController?.popViewController(animated: true)
+    }
+
+}
